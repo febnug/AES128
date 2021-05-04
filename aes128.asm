c31af855b0b558ef276910f77fbfeffe2c6cf6f5
Febriyanto Nugroho <47207224+febnug@users.noreply.github.com>
2021-05-04 20:15:43 +0700

    Update aes128.asm

--- aes128.asm
+++ aes128.asm
@@ -1,5 +1,7 @@
 ; AES-128 asm
 ;
+; (c) Febriyanto Nugroho
+;
 ;
 ; compile dengan :
 ;
@@ -18,33 +20,33 @@ section .text
 
 _start:
 
-pxor    xmm2, xmm2
-pxor    xmm3, xmm3
-mov     bx, 0x36e5
-mov     ah, 0x73
+      pxor    xmm2, xmm2
+      pxor    xmm3, xmm3
+      mov     bx, 0x36e5
+      mov     ah, 0x73
 
 roundloop:
-shr     ax, 7
-div     bl
-mov     byte [loc_7cb9+5], ah  
+      shr     ax, 7
+      div     bl
+      mov     byte [loc_7cb9+5], ah  
 
 loc_7cb9:
-aeskeygenassist xmm1, xmm0, 0x45
-pshufd  xmm1, xmm1, 0xff
+      aeskeygenassist xmm1, xmm0, 0x45
+      pshufd  xmm1, xmm1, 0xff
 
 shuffle:
-shufps  xmm2, xmm0, 0x10
-pxor    xmm0, xmm2
-xor     byte [shuffle+3], 0x9c
-js      short shuffle
+      shufps  xmm2, xmm0, 0x10  
+      pxor    xmm0, xmm2
+      xor     byte [shuffle+3], 0x9c
+      js      short shuffle
 
-pxor    xmm0, xmm1
-cmp     ah, bh
-jz      short lastround
+      pxor    xmm0, xmm1
+      cmp     ah, bh
+      jz      short lastround
 
-aesenc  xmm3, xmm0
-jmp     short roundloop
+      aesenc  xmm3, xmm0
+      jmp     short roundloop
 
 lastround:
-aesenclast xmm3, xmm0
-ret
+      aesenclast xmm3, xmm0
+      ret
